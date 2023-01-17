a7adc205c4a9f37d1b4617c79343c8a2e7fcb8a6
Bruno Silva <bruno.silva@catskillet.com>
2023-01-17 16:16:01 -0300

    feat: home criado, adicionado header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuHeaderComponent } from './components/menu-header/menu-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/menu-header/menu-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Menu',
+  templateUrl: './menu-header.component.html',
+  styleUrls: ['./menu-header.component.scss']
+})
+export class MenuHeaderComponent {
+
+}
