2cce4056d6ae7ae3fc8306af47a9cb42893500db
Gabriel Leao <gabriel.leao@catskillet.com>
2023-01-19 09:00:10 -0300

    fix: campo de busca

--- src/app/components/menu-header/menu-header.component.ts
+++ src/app/components/menu-header/menu-header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PokemonsService } from 'src/app/services/pokemons.service';
 
 @Component({
   selector: 'Menu',
   templateUrl: './menu-header.component.html',
-  styleUrls: ['./menu-header.component.scss']
+  styleUrls: ['./menu-header.component.scss'],
 })
 export class MenuHeaderComponent {
+  onSearch(name: string) {
+    this.pokeService
+      .getPokemonInfo(name.toLocaleLowerCase())
+      .subscribe((pokemon) =>
+        this.router.navigateByUrl(`/pokemon/${pokemon.name}`)
+      );
+  }
 
+  constructor(private pokeService: PokemonsService, private router: Router) {}
 }
